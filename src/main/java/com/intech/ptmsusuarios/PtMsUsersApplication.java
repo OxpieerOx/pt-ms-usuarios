fabe609a0ad0e20ea92187fbdcb621623f125c05
Pieer Rodas <pie_er1@hotmail.com>
2024-01-25 15:09:21 -0600

    initial project

---
+++ src/main/java/com/intech/ptmsusuarios/PtMsUsersApplication.java
@@ -0,0 +1,13 @@
+package com.intech.ptmsusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtMsUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtMsUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intech/ptmsusuarios/PtMsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intech.ptmsusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtMsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
